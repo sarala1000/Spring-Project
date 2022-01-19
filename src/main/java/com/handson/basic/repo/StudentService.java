78bd3a8596ca22e2bf8ff9e9056a5f01e25fafb4
Niv Itzhaky <niv.itzhaky@gmail.com>
2022-01-19 14:19:57 +0200

    with FPS

--- src/main/java/com/handson/basic/repo/StudentService.java
+++ src/main/java/com/handson/basic/repo/StudentService.java
@@ -4,6 +4,7 @@ import com.handson.basic.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -11,6 +12,10 @@ public class StudentService {
 
     @Autowired
     StudentRepository repository;
+
+    public List<Student> getStudentWithSatHigherThan(Integer sat) {
+        return repository.findAllBySatScoreGreaterThan(sat);
+    }
 
     public Iterable<Student> all() {
         return repository.findAll();
